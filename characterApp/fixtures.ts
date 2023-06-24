116bd4439c1a38b8752666dd5662f04ba66e6c55
Ron Fenton <fentonron@live.com.au>
2023-06-24 11:07:59 +0200

    Types

---
+++ characterApp/fixtures.ts
@@ -0,0 +1,27 @@
+import { Attribute, Character } from "./types";
+
+export const attribute = (x:Partial<Attribute>):Attribute => {
+  const a:Attribute = {
+    name: 'Example Attribute',
+    description: 'A token example **here**.',
+    base: '10',
+    lvlBase: 10,
+    lvlMod: 0,
+    lvlPurchase: 0,
+    lvl:0,
+    ...x
+  }
+  return a
+}
+
+export const character = (x:Partial<Character>):Character => {
+  const c:Character = {
+    attributes: {},
+    skills: {},
+    items: {},
+    version: { current: "0", last: "-1" },
+    id: "0",
+    ...x
+  }
+  return c
+}
--- characterApp/types.ts
+++ characterApp/types.ts
@@ -1,16 +1,70 @@
-export type Character = {
+export type CharacterState = {
+  character: Character
+  registry: any
+}
 
+export type Character = {
+  id: string
+  version: Version
+  attributes: {
+    [key:string]: Attribute
+  }
+  skills: {
+    [key:string]: Skill
+  }
+  items: {
+    [uuid:string]: InventoryItem
+  }
+}
+
+export type Version = {
+  current: string
+  last: string
 }
 
 export type Characteristic = {
   name: string,
-  library?: {
-
-  }
+  library?: LibraryReference,
+  description: string,
 }
 
 export type LibraryReference = {
   libraryId: string
   objectId: string
+}
+
+export type PermissionList = {
+  ownerId: string
+  gmId: string
+  viewerIds: string[]
+  editorIds: string[]
+}
+
+export type Identity = {
+  nickname?: string
+  fullName: string
+  description: string
+}
+
+export type Levelled = {
+  base: string
+  lvlBase: number
+  lvlPurchase: number
+  lvlMod: number
+  lvl: number
+}
+
+export type Attribute = Characteristic & Levelled & {
+  abbreviation?: string
+}
+
+export type Skill = Characteristic & Levelled & {
+  difficulty: number
   
+}
+
+export type InventoryItem = Characteristic & {
+  qty: number
+  wt: number
+  val: number
 }
